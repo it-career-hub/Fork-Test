5c80f7b2c8ace591d92b053cd0bb6d8d32110548
meni0419 <meni0419@gmail.com>
2025-02-25 12:03:01 +0200

    added another print

--- MaksymM.py
+++ MaksymM.py
@@ -4,5 +4,8 @@ try:
     from termcolor import colored
 
     print(colored("Hello, World!", "cyan", attrs=["bold", "underline"]))
+    print(colored("This repository showcases beautiful terminal outputs!", "green", attrs=["bold"]))
 except ImportError:
     print("Hello, World! (Install 'termcolor' for a more beautiful output)")
+    print("This repository showcases beautiful terminal outputs! (Install 'termcolor' for a more beautiful output)")
+
